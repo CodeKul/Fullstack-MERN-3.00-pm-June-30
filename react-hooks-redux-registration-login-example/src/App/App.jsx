9900c00a39abd657a98baca8f8d1d80e650e566e
sanketcodekul <sanketbhosale.codekul@gmail.com>
2021-11-25 15:26:40 +0530

    React - Redux

---
+++ react-hooks-redux-registration-login-example/src/App/App.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
+
+// https://hashnode.com/post/reactjs-higher-order-components-in-plain-english-cjr7n30pk00c0k4s1dj7dctoh
+// https://www.pluralsight.com/guides/how-to-wrap-one-react-component-into-another
+
+// https://github.com/darwin619/Netflix-Clone/tree/master/src/Redux/User
+
+function App() {
+    const alert = useSelector(state => state.alert);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }, []);
+
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <div className="col-md-8 offset-md-2">
+                    {alert.message &&
+                        <div className={`alert ${alert.type}`}>{alert.message}</div>
+                    }
+                    <Router history={history}>
+                        <Switch>
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <Route path="/login" component={LoginPage} />
+                            <Route path="/register" component={RegisterPage} />
+                            <Redirect from="*" to="/" />
+                        </Switch>
+                    </Router>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { App };
--- usereducer-example/src/App.js
+++ usereducer-example/src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import CounterRed from './reducer/reducerCount';
 
+// https://blog.logrocket.com/react-pure-components-functional/
+
+// https://epicreact.dev/memoization-and-react/
+
+// https://www.qed42.com/insights/coe/javascript/optimize-your-react-app-performance-memoization
+
+// https://blog.logrocket.com/why-use-redux-reasons-with-clear-examples-d21bffd5835/
+
 function App() {
   return (
     <div className="App">
       <CounterRed/>
+
+
+     
     </div>
   );
 }
